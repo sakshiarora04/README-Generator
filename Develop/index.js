17abd3e5f1be0fdd337b901ead3b53468ea54183
Your Name <sakshiarora245gmail.comqqaaa git config --global user.email sakshiarora245gmail.com>
2023-07-22 23:40:52 +1000

    index.js propmt

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,89 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your GitHub username ? ", "What is your email address ? ", "What is your project's name ?", "Please write a short description of your project.", "What kind of license should your project have ?", "What command should be run to install dependencies ? ", "What command should be run to run tests ?", "What does the user need to know about using the repo ? ", "What does the user need to know about contributing to the repo ?"];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const fullFileName=`./output/${fileName}`
+   
+    const readmePageContent = generateMarkdown(data);
+    console.log(data)
+    fs.writeFile(fullFileName, readmePageContent,(err) =>
+    err ? console.log(err) : console.log('Successfully created README.md')
+  );
+}
 
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'username',
+        message: questions[0],
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: questions[1],
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: questions[2],
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: questions[3],
+      },
+      {
+        type: 'list',
+        name: 'licence',
+        message: questions[4],
+        choices:[{ name: 'MIT' },
+        { name: 'APACHE 2.0' },
+        { name: 'GPL 3.0' },
+        { name: 'BSD 3' },
+        { name: 'none' },],
+      },
+      {
+        type: 'input',
+        name: 'dependencies',
+        message: questions[5],
+        default: 'npm inquirer@^8.2.4',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: questions[6],
+        default: 'npm test',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: questions[7],
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: questions[8],
+      },
+    ]);
+  }
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptUser()
+      
+        .then((answers)=>{
+         
+            writeToFile("README.md",answers);
+        })
+        .catch((err) =>
+         err ? console.log(err) : console.log('Successfully created README.md!'));
+  
+}
 
 // Function call to initialize app
 init();
